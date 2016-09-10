ed4570e9e49f11a3f9343a01f021b2107e7a07a1
ShadowfeindX <shadowfeind@programmer.net>
2016-09-09 23:52:15 -0400

    program completed

--- kg-lbs.cpp
+++ kg-lbs.cpp
@@ -6,8 +6,20 @@ double RATIO = 2.20462262;
 using namespace std;
 
 int main() {
+
+    //Declare variables
+    double weight_kg, weight_lbs;
+
     // Get mass in kilograms from user
-    // Convert kg to lbs using ration constant
+    cout << "What is your mass in kilograms? ";
+    cin >> weight_kg;
+
+    // Convert kg to lbs using ratio constant
+    weight_lbs = weight_kg * RATIO;
+
     // Output weight in pounds
+    cout << "You weigh approximately: "
+         << weight_lbs << " lbs" << endl;
+    // END
 	return 0;
 }
--- kg-lbs.cxx
+++ kg-lbs.cxx
@@ -1,15 +1,15 @@
 #include <iostream>
 
-double RATIO = 2.20462262;
+float RATIO = 2.20462262;
 
 using namespace std;
 
 int main() {
-	
-	double w;
-	
+
+	float w;
+
 	cout << "Enter mass: "; cin >> w;
 	cout << "Your weight is " << w*RATIO << " lbs." << endl;
-    
+
 	return 0;
 }
