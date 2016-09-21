162855f3798ff17d2f4b449ee085eed1e4738a40
ShadowfeindX <shadowfeind@programmer.net>
2016-09-21 01:28:33 -0400

    spaghetti completed

--- alpha.cxx
+++ alpha.cxx
@@ -22,11 +22,20 @@
  */
 
 
-#include <stdio.h>
+#include <cmath>
+#include <iostream>
 
-int main(int argc, char **argv)
+using namespace std;
+int main(int argc, char *argv[])
 {
-	
-	return 0;
+    double alpha[50];
+    for (int i = 0; i<25; ++i) {
+        alpha[i] = pow(i,2);
+    } for (int i = 25; i<50; i++) {
+        alpha[i] = 3*i;
+    } for (int i=0;i<50;i++) {
+        if (i%10==0) cout << endl;
+        cout << alpha[i] << ' ';
+    } cout << endl << endl;
+    return 0;
 }
-
