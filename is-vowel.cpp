0d16b27ae2aacaab02d2720996c1a9e7593635cd
ShadowfeindX <the_specialist@programmer.net>
2016-10-25 14:17:02 -0400

    completed

--- is-vowel.cpp
+++ is-vowel.cpp
@@ -1,32 +1,21 @@
-/*
- * main.cpp
- * 
- * Copyright 2016 ShadowfeindX <shadowfeind@programmer.net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- * 
- */
-
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, char **argv)
-{
+// Check if string vowels contans character c
+bool isVowel(char& c, string& v){if (v.find(c) != string::npos) return true;}
+
+int main() {
+	// Create string of vowels
+	string vowels("AaEeIiOoUu");
 	
+	// Get input form user
+	cout << "Enter a string: ";
+	string s;int i = 0;getline(cin,s);
+	
+	// Iterate through all characters in input
+	for (char c : s) if (isVowel(c, vowels)) ++i;
+	
+	// Output # of vowels
+	cout << i << " vowels" << endl;
 	return 0;
 }
-
