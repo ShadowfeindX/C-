61a5c66d69e44a34b9d50431b5152c8333363838
ShadowfeindX <the_specialist@programmer.net>
2016-10-27 13:43:38 -0400

    completed

--- growing-population.cpp
+++ growing-population.cpp
@@ -1,32 +1,24 @@
-/*
- * main.cpp
- * 
- * Copyright 2016 ShadowfeindX <shadowfeind@programmer.net>
- * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- * 
- */
-
-
 #include <iostream>
-
-int main(int argc, char **argv)
-{
-	
+using namespace std;
+int main() {
+	char c;	float x[2], y[2];
+	// Prompt user for formatted input
+	cout << "Please enter population data in (P,g) format where" << endl
+		 << "P is population and g is the current growth rate as a percentage.\n" << endl
+		 << "\nPress Enter to Start!" << endl;
+	// Wait for input
+	getchar(); if (system("CLS")) system("clear");
+	// Get user input
+	cout << "Town A: ";	cin >> c >> y[0] >> c >> x[0] >> c;
+	cout << "Town B: ";	cin >> c >> y[1] >> c >> x[1] >> c;
+	// Calculate growth rates
+	if (system("CLS")) system("clear");	x[0] /= 100; x[1] /= 100;
+	// Display population for every year that A < B
+	for (c=0; y[1] > y[0]; y[0]+=y[0]*x[0], y[1]+=y[1]*x[1], c++ ) {
+		printf ("Year %i: A -> %g B-> %g\n", c, y[0], y[1]);
+	// Display population for year A > B
+	};	printf ("Year %i: A -> %g B-> %g\n", c, y[0], y[1]);
+	// Display final output for user
+	printf ("\nAfter Year %i, Town A will have a greater population than Town B!\n", c-1);
 	return 0;
 }
-
